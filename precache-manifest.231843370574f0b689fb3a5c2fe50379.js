e1b6244904151d45fc38be32d75e0d2879faf26e
黃首翰（ArvinH） <arvin0731@gmail.com>
2018-12-15 22:20:47 +0800

    Updates

---
+++ precache-manifest.231843370574f0b689fb3a5c2fe50379.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c84253f8c8ff6e24a419",
+    "url": "/YearEndReview-2018/static/js/runtime~main.c84253f8.js"
+  },
+  {
+    "revision": "ce8ce066f30aed650acd",
+    "url": "/YearEndReview-2018/static/js/main.ce8ce066.chunk.js"
+  },
+  {
+    "revision": "a8fdc3d0a3b641036ea3",
+    "url": "/YearEndReview-2018/static/js/1.a8fdc3d0.chunk.js"
+  },
+  {
+    "revision": "ce8ce066f30aed650acd",
+    "url": "/YearEndReview-2018/static/css/main.43523cfb.chunk.css"
+  },
+  {
+    "revision": "4163ebea6e3ae4bd95908c4201448422",
+    "url": "/YearEndReview-2018/index.html"
+  }
+];
